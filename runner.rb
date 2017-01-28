42b494b38f657bd658f749e23a3379fd58b6cc07
bontgoy <tyjkeating@gmail.com>
2017-01-28 11:06:08 -0600

    added andrea to the bingo runner file

--- runner.rb
+++ runner.rb
@@ -1,7 +1,7 @@
 require_relative 'game_file'
 require_relative 'board_file'
 
-new_game = Game.new(["Ty", "Andrew", "Emmanuel", "Lucas", "Jen"])
+new_game = Game.new(["Ty", "Andrea", "Andrew", "Emmanuel", "Lucas", "Jen"])
 
 100.times do 
 	new_game.draw
