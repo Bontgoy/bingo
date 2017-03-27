cc68469c037d0fdeb6de49e5bf8d3cb49d9ddd9e
bontgoy <tyjkeating@gmail.com>
2017-03-27 16:18:13 -0600

    Adde Lisa back to the branch.

--- runner.rb
+++ runner.rb
@@ -1,7 +1,7 @@
 require_relative 'game_file'
 require_relative 'board_file'
 
-new_game = Game.new(["Ty", "Andrew", "Emmanuel", "Lucas", "Glenna"])
+new_game = Game.new(["Ty", "Andrew", "Emmanuel", "Lucas", "Glenna", "Lisa"])
 
 100.times do
 	new_game.draw
